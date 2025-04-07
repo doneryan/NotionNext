f5238f1c57352e6edbb1fa176c86bcf950251ecc
doneryan <117269564+doneryan@users.noreply.github.com>
2025-04-07 19:03:29 +0800

    Update config.js
    焦点文字修改

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://www.izhiku.edu.kg' },
+    { title: '欢迎来到小月半子的博客', url: 'https://www.izhiku.edu.kg' },
     { title: '访问文档中心获取更多帮助', url: 'https://www.izhiku.edu.kg' }
   ],
 
@@ -18,7 +18,7 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_1: '分享知识',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'IZHIKU.COM',
   HEO_HERO_TITLE_4: '新版上线',
